4041a82e5a1bf54e9e18c485b5e190ff11fcca02
cjallow001 <cjallow@hotmail.com>
2022-10-17 23:27:37 -0500

    Module 3 Challenge - Python

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,11 +10,11 @@
 import os
 import csv
 
-# Here we Define the PyBank variables
+# PyBank variables
 Total_months = []
 profit_loss_changes = []
 
-count_months = 0
+months_count = 0
 net_profit_loss = 0
 previous_month_profit_loss = 0
 current_month_profit_loss = 0
@@ -27,40 +27,59 @@ budget_data_csv = os.path.join("Resources", "budget_data.csv")
 with open(budget_data_csv, newline="") as csvfile:
     csv_reader = csv.reader(csvfile, delimiter=",")
 
+    csv_header = next(csvfile)
+
     # Read through each row of data after the header
     for row in csv_reader:
+        months_count += 1
+        current_month_profit_loss = int(row[1])
+        net_profit_loss += current_month_profit_loss
+        if(months_count == 1):
 
-    count_months += 1
+            # Make the value of previous month to be equal to current month
+            previous_month_profit_loss = current_month_profit_loss
+            continue
+        else:
+            profit_loss_change = current_month_profit_loss - previous_month_profit_loss
+            Total_months.append(row[0])
 
-    current_month_profit_loss = int(row[1])
-    net_profit_loss += current_month_profit_loss
+            profit_loss_changes.append(profit_loss_change)
+            previous_month_profit_loss = current_month_profit_loss
 
+    # Profit/Losses over the entire period
+    sum_profit_loss = sum(profit_loss_changes)
+    average_profit_loss = round(sum_profit_loss/(months_count - 1), 2)
 
+    # highest/lowest changes Profit/Losses over the entire period
+    top_change_amount = max(profit_loss_changes)
+    low_change_amount = min(profit_loss_changes)
 
+    highest_month_index = profit_loss_changes.index(top_change_amount)
+    lowest_month_index = profit_loss_changes.index(low_change_amount)
 
-
-
-
+    # Assign Greatest increase and decrease month
+    greatest_increase_month = Total_months[highest_month_index]
+    greatest_decrease_month = Total_months[lowest_month_index]
 
 
 # print the analysis to the terminal
 print("Financial Analysis")
 print("----------------------------")
-print(f"Total Months:  {count_months}")
+print(f"Total Months:  {months_count}")
 print(f"Total:  ${net_profit_loss}")
 print(f"Average Change:  ${average_profit_loss}")
-print(f"Greatest Increase in Profits:  {best_month} (${highest_change})")
-print(f"Greatest Decrease in Losses:  {worst_month} (${lowest_change})")
+print(f"Greatest Increase in Profits:  {greatest_increase_month} (${top_change_amount})")
+print(f"Greatest Decrease in Profits:  {greatest_decrease_month} (${low_change_amount})")
 
 
 # export a text file with the results
-budget_file = os.path.join("Output", "budget_data.txt")
-with open(budget_file, "w") as outfile:
+budget_file = os.path.join("..", "analysis", "budget_data.txt")
+with open(budget_file, "w") as out_file:
 
-    outfile.write("Financial Analysis\n")
-    outfile.write("----------------------------\n")
-    outfile.write(f"Total Months:  {count_months}\n")
-    outfile.write(f"Total:  ${net_profit_loss}\n")
-    outfile.write(f"Average Change:  ${average_profit_loss}\n")
-    outfile.write(f"Greatest Increase in Profits:  {best_month} (${highest_change})\n")
-    outfile.write(f"Greatest Decrease in Losses:  {worst_month} (${lowest_change})\n")
+    out_file.write("Financial Analysis\n")
+    out_file.write("----------------------------\n")
+    out_file.write(f"Total Months:  {months_count}\n")
+    out_file.write(f"Total:  ${net_profit_loss}\n")
+    out_file.write(f"Average Change:  ${average_profit_loss}\n")
+    out_file.write(f"Greatest Increase in Profits:  {greatest_increase_month} (${top_change_amount})\n")
+    out_file.write(f"Greatest Decrease in Profits:  {greatest_decrease_month} (${low_change_amount})\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,12 +6,69 @@
 # 5. The winner of the election based on popular vote
 # 6. The final script should both print the analysis to the terminal and export a text file with the results.
 
-# Import dependencies
+# Dependencies
 import os
 import csv
 import collections
 from collections import Counter
-
-# Define PyPoll's variables
 voters_candidates = []
 votes_per_candidate = []
+election_data_csv = os.path.join("Resources", "election_data.csv")
+
+# Open and read csv
+with open(election_data_csv, newline="") as csvfile:
+
+    csv_reader = csv.reader(csvfile, delimiter=",")
+
+    # Read the header row first
+    csv_header = next(csvfile)
+
+    for row in csv_reader:
+
+        voters_candidates.append(row[2])
+
+    sorted_list = sorted(voters_candidates)
+    
+    arrange_list = sorted_list
+
+    count_candidate = Counter (arrange_list) 
+    votes_per_candidate.append(count_candidate.most_common())
+
+    # The percentage of votes each candidate won
+    for percentwon in votes_per_candidate:
+       
+        one1 = format((percentwon[0][1])*100/(sum(count_candidate.values())),'.3f')
+        two2 = format((percentwon[1][1])*100/(sum(count_candidate.values())),'.3f')
+        three3 = format((percentwon[2][1])*100/(sum(count_candidate.values())),'.3f')
+        four4 = format((percentwon[3][1])*100/(sum(count_candidate.values())),'.3f')
+
+    
+# Print the analysis to the terminal
+print("Election Results")
+print("---------------------------------")
+print(f"Total Votes:  {sum(count_candidate.values())}")
+print("---------------------------------")
+print(f"{votes_per_candidate[0][0][0]}: {one1}% ({votes_per_candidate[0][0][1]})")
+print(f"{votes_per_candidate[0][1][0]}: {two2}% ({votes_per_candidate[0][1][1]})")
+print(f"{votes_per_candidate[0][2][0]}: {three3}% ({votes_per_candidate[0][2][1]})")
+print(f"{votes_per_candidate[0][3][0]}: {four4}% ({votes_per_candidate[0][3][1]})")
+print("--------------------------------")
+print(f"Winner:  {votes_per_candidate[0][0][0]}")
+print("--------------------------------")
+
+
+# Export a text file with the results
+election_file = os.path.join("..", "analysis", "election_data.txt")
+with open(election_file, "w") as out_file:
+
+    out_file.write("Election Results\n")
+    out_file.write("--------------------------------\n")
+    out_file.write(f"Total Votes:  {sum(count_candidate.values())}\n")
+    out_file.write("--------------------------------\n")
+    out_file.write(f"{votes_per_candidate[0][0][0]}: {one1}% ({votes_per_candidate[0][0][1]})\n")
+    out_file.write(f"{votes_per_candidate[0][1][0]}: {two2}% ({votes_per_candidate[0][1][1]})\n")
+    out_file.write(f"{votes_per_candidate[0][2][0]}: {three3}% ({votes_per_candidate[0][2][1]})\n")
+    out_file.write(f"{votes_per_candidate[0][3][0]}: {four4}% ({votes_per_candidate[0][3][1]})\n")
+    out_file.write("--------------------------------\n")
+    out_file.write(f"Winner:  {votes_per_candidate[0][0][0]}\n")
+    out_file.write("--------------------------------\n")    
